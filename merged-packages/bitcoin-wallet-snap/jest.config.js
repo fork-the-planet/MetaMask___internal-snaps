bbf501d56364d6cf8f6023ca3c1caa107427d727
Michele Esposito <34438276+mikesposito@users.noreply.github.com>
2026-02-10 16:00:03 +0100

    test: enable coverage collection and thresholds (#586)

--- merged-packages/bitcoin-wallet-snap/jest.config.js
+++ merged-packages/bitcoin-wallet-snap/jest.config.js
@@ -3,6 +3,34 @@
  * @type {import('ts-jest').JestConfigWithTsJest}
  */
 const config = {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['./src/**/*.ts', './src/**/*.tsx'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: ['.*/index\\.ts'],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'babel',
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: ['text', 'html', 'json-summary', 'lcov'],
+
+  // An object that configures minimum threshold enforcement for coverage results
+  coverageThreshold: {
+    global: {
+      branches: 65.5,
+      functions: 62.64,
+      lines: 75.29,
+      statements: 74.57,
+    },
+  },
+
   preset: '@metamask/snaps-jest',
   transform: {
     '^.+\\.(t|j)sx?$': 'ts-jest',
